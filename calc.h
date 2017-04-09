a49641aeb1b8ede3187c010ca7e243283124f896
Chou Cheng-Yao <anti.ning@gmail.com>
2017-04-09 07:43:22 -0500

    加入complex class

--- calc.h
+++ calc.h
@@ -1,5 +1,5 @@
 #pragma once
-//#include <iostream>
+#include <iostream>
 #include <algorithm>
 #include <cstdint>
 #include <string>
@@ -8,34 +8,45 @@
 
 const uint64_t BaseMax = 1000000000000000000, subtrahend = BaseMax-1;
 const uint32_t SizeMax = 100, BaseLen = 18, ContainLenMax = SizeMax * BaseLen;
-class Fraction;
-class BigInt{
+class Decimal;
+class Integer{
 private:
     uint64_t _digi[SizeMax];
     uint32_t _sizeUsed;
     bool _sign;
 public:
-    BigInt(){}
-    BigInt(std::string);
+    Integer(){}
+    Integer(std::string);
     
-    friend const BigInt operator+(const BigInt&, const BigInt&);
-    friend const BigInt operator-(const BigInt&, const BigInt&);
-    friend const BigInt operator*(const BigInt&, const BigInt&);
-    friend const Fraction operator/(const BigInt&, const BigInt&);
-    friend const bool operator>(const BigInt&, const BigInt&);
-    friend const bool operator<(const BigInt&, const BigInt&);
-    friend const bool operator==(const BigInt&, const BigInt&);
-    friend const BigInt GCD(const BigInt&, const BigInt&);
+    friend const Integer operator+(const Integer&, const Integer&);
+    friend const Integer operator-(const Integer&, const Integer&);
+    friend const Integer operator*(const Integer&, const Integer&);
+    friend const Decimal operator/(const Integer&, const Integer&);
+    friend const bool operator>(const Integer&, const Integer&);
+    friend const bool operator<(const Integer&, const Integer&);
+    friend const bool operator==(const Integer&, const Integer&);
+    friend const Integer GCD(const Integer&, const Integer&);
+    friend const std::ostream operator<<(std::ostream& stream, const Integer&);
+    friend const std::istream operator>>(std::istream& stream, const Integer&);
     
     void Complete();
     bool isZero() const;
     std::string ToString() const;
 };
 
-class Fraction{
+class Decimal{
 private:
-    BigInt _numerator, _denominator;
+    Integer _numerator, _denominator;
     bool _sign;
 public:
-    Fraction(){}
+    Decimal(){}
+    
+};
+
+class Complex{
+private:
+    Decimal _realPart, _imaginePart;
+public:
+    Complex(){}
+    
 };
