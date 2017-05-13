1de64c8ed91601ef50fa63d13b37dd95502c344d
Chou Cheng-Yao <anti.ning@gmail.com>
2017-05-13 16:07:28 +0800

    Add files via upload

--- Integer.hpp
+++ Integer.hpp
@@ -1,9 +1,14 @@
 //
-//  Integer.cpp
+//  Integer.hpp
 //  CMDMod
 //
 //  Created by 周政耀 on 2017/5/11.
 //  Copyright © 2017年 周政耀. All rights reserved.
 //
 
-#include "Integer.hpp"
+#ifndef Integer_hpp
+#define Integer_hpp
+
+#include <stdio.h>
+
+#endif /* Integer_hpp */
